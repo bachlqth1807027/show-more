0fbc2aa6cd23d9421492be239375725b657667ec
bachlqth1807027 <bachlqth1807027@fpt.edu.vn>
2018-11-10 18:25:48 +0700

    baithi

--- baithi.js
+++ baithi.js
@@ -1,28 +1,6 @@
 var btnsubmit = document.forms["register-form"]["btn-submit"];
 btnsubmit.onclick = function (){
-	var txtusername = document.forms["register-form"]["username"];
-	var name = txtusername.nextElementSibling;
-	if (txtusername == null || txtusername.value.length == 0) {
-		name.innerHTML = "*Vui lòng nhập tên của bạn. ";
-		name.classList.remove("hidden-text");
-	}else if (txtusername.value.length < 5){
-		name.innerHTML = "*Tên của bạn quá ngắn vui lòng nhập lại.";
-		name.classList.remove("hidden-text");
-	}else if (txtusername.value.length > 20){
-		name.innerHTML = "*Tên của bạn quá dài vui lòng nhập lại.";
-		name.classList.remove("hidden-text");
-	}else {
-		name.innerHTML = "*Tên hợp lệ.";
-		name.classList.remove("hidden-text");
-		name.classList.add("succes-text");
-	}
-	var txtpassword = document.forms["register-form"]["password"];
-	var	pass = txtpassword.nextElementSibling;
-	if (txtpassword == null || txtpassword.value.length == 0) {
-		pass.innerHTML = "* Không được bỏ trông mật khẩu.";
-		pass.classList.remove("hidden-text");
-	}
-	var txtfullname = document.forms["register-form"]["fullName"];
+var txtfullname = document.forms["register-form"]["fullName"];
 	var	fullname = txtfullname.nextElementSibling;
 	if(txtfullname == null || txtfullname.value.length == 0){
 		fullname.innerHTML = "* Không dược bỏ trông tên đầy đủ.";
@@ -39,5 +17,11 @@ btnsubmit.onclick = function (){
 	if (txtemail == null || txtemail.value.length==0) {
 		mail.innerHTML = "* Không được để trông email.";
 		mail.classList.remove("hidden-text");
+	}
+	var	txtphone = document.forms["register-form"]["phone"];
+	var phone = txtphone.nextElementSibling;
+	if (txtphone == null || txtphone.value.length == 0) {
+		phone.innerHTML = "*Không được để trống số điện thoại";
+		phone.classList.remove("hidden-text");
 	}
 }
